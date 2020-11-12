f9547011bee15194541a67d6ebb8e9897c9fba4e
Santhosh-N <59107079+Santhosh-N@users.noreply.github.com>
2020-11-12 04:42:07 +0000

    Add files via upload

---
+++ MissileMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMovement : MonoBehaviour
+{
+    public Transform Target;
+
+    public float Speed;
+    public float Acceleration;
+
+    Rigidbody2D rb;
+
+    public float RotationControl;
+
+    float MovY, MovX = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovY = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 Direction = transform.position - Target.position;
+
+        Direction.Normalize();
+
+        float cross = Vector3.Cross(Direction, transform.right).z;
+
+        rb.angularVelocity = RotationControl * cross;
+
+
+        Vector2 Vel = transform.right * (MovX * Acceleration);
+        rb.AddForce(Vel);
+
+        float Dir = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.right));
+
+        float trustForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.down)) * 2.0f;
+
+        Vector2 relForce = Vector2.up * trustForce;
+
+        rb.AddForce(rb.GetRelativeVector(relForce));
+
+        if(rb.velocity.magnitude > Speed)
+        {
+            rb.velocity = rb.velocity.normalized * Speed;
+        }
+
+    }
+}
